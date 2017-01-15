64e4650ebbc819490b5178fe5e3c466fa9424f63
Daven Quinn <dev@davenquinn.com>
2017-01-15 00:47:49 -0500

    Make everything conform to a standard CLI interface

--- figurator/cli.py
+++ figurator/cli.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import click, re
+from functools import update_wrapper
 from . import collect_figures
 from .figure_list import create_latex_figure_list
 from .inline_figures import inline_figure_filter
@@ -19,36 +20,35 @@ def collect(defs, collect_dir, search_dirs, copy=False):
         search_dirs,
         copy=copy)
 
+
+def standard_interface(f):
+    @click.argument('defs', type=_path)
+    @click.option('--captions',type=_path)
+    @click.option('--collect-dir',type=_path)
+    @click.option('--template-dir',type=_path)
+    @click.option('--starred-floats/--no-starred-floats', default=True)
+    @click.option('--natbib','backend',flag_value='natbib', default=True)
+    @click.option('--biblatex', 'backend',flag_value='biblatex')
+    @click.pass_context
+    def cli_wrapper(ctx, defs, **kwargs):
+        return ctx.invoke(f, ctx, defs, **kwargs)
+    return update_wrapper(cli_wrapper, f)
+
 @figures.command(name='list')
-@click.argument('defs', type=_path)
-@click.option('--captions',type=_path)
-@click.option('--collect-dir',type=_path)
-@click.option('--starred-floats/--no-starred-floats', default=True)
-@click.option('--natbib','backend',flag_value='natbib', default=True)
-@click.option('--biblatex', 'backend',flag_value='biblatex')
-def figure_list(defs, captions=None, collect_dir=None, starred_floats=True, backend='natbib'):
+@standard_interface
+def figure_list(ctx, defs, **kwargs):
     create_latex_figure_list(defs, captions=captions, collect_dir=collect_dir,
         starred_floats=starred_floats, citation_backend=backend)
 
 @figures.command(name='inline')
-@click.argument('defs', type=_path)
-@click.option('--captions',type=_path)
-@click.option('--collect-dir',type=_path)
-@click.option('--template-dir',type=_path)
-@click.option('--starred-floats/--no-starred-floats', default=True) # Use starred floats for two-column layouts
-@click.option('--natbib','backend',flag_value='natbib', default=True)
-@click.option('--biblatex', 'backend',flag_value='biblatex')
-def figure_list(defs, captions=None, collect_dir=None, template_dir=None, starred_floats=True, backend='natbib'):
+@standard_interface
+def figure_list(ctx, defs, **kwargs):
     """
     A text filter to include inline figures in pandoc markdown
     Pipe text through this filter then into pandoc
     """
     stdin = click.get_text_stream('stdin')
     stdout = click.get_text_stream('stdout')
-    fn = inline_figure_filter(defs, captions=captions,
-                collect_dir=collect_dir,
-                template_dir=template_dir,
-                starred_floats=starred_floats,
-                citation_backend=backend)
+    fn = inline_figure_filter(defs, **kwargs)
     text = stdin.read()
     stdout.write(fn(text))
