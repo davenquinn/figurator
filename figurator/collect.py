5444ae5b194a24a1aa7ed4830401fc3208f4ec9b
Daven Quinn <dev@davenquinn.com>
2022-12-07 10:46:25 -0600

    Fixed incorrect symlink generation in collect

--- figurator/collect.py
+++ figurator/collect.py
@@ -5,26 +5,29 @@ from click import echo, secho, style
 from .processors import load_spec, collected_filename
 
 ### Collection of figures ###
-_file = lambda x: style(x, fg='cyan')
-_bullet = lambda c: style("●",fg=c)
+_file = lambda x: style(x, fg="cyan")
+_bullet = lambda c: style("●", fg=c)
 
-def find_file(file,search_paths=[]):
+
+def find_file(file, search_paths=[]):
     for p in search_paths:
-        fn = path.join(p,file)
+        fn = path.join(p, file)
         if path.isfile(fn):
             return fn
-    echo(_bullet('red')+" Couldn't find file "+_file(file), err=True)
+    echo(_bullet("red") + " Couldn't find file " + _file(file), err=True)
+
 
 def find_files(cfg, search_paths):
     __ = []
-    if 'files' in cfg:
-        __ += list(cfg['files'])
-    if 'file' in cfg:
-        __.append(cfg['file'])
+    if "files" in cfg:
+        __ += list(cfg["files"])
+    if "file" in cfg:
+        __.append(cfg["file"])
     for file in __:
         fi = find_file(file, search_paths)
         if fi:
             yield fi
+
 
 def resolve_figures(spec, search_paths=[]):
     spec = load_spec(spec)
@@ -32,11 +35,13 @@ def resolve_figures(spec, search_paths=[]):
         for f in find_files(cfg, search_paths):
             yield f
 
+
 def relative_symlink(src, dst):
     dir_ = path.dirname(dst)
     src1 = path.relpath(src, dir_)
-    dst1 = path.join(dir_, path.basename(src1))
+    dst1 = path.join(dir_, path.basename(dst))
     return symlink(src1, dst1)
+
 
 def collect_figures(spec, outdir, search_paths=[], copy=False):
     """
@@ -46,7 +51,7 @@ def collect_figures(spec, outdir, search_paths=[], copy=False):
     outdir: directory in which to collect figures
     search_paths: dirs in which to search for figures matching filenames
     """
-    secho("Collecting figures from "+_file(spec), bold=True, err=True, fg='cyan')
+    secho("Collecting figures from " + _file(spec), bold=True, err=True, fg="cyan")
 
     spec = load_spec(spec)
     if spec is None:
@@ -54,24 +59,24 @@ def collect_figures(spec, outdir, search_paths=[], copy=False):
 
     for cfg in spec:
         for fn in find_files(cfg, search_paths):
-            new_fn = collected_filename(cfg,outdir)
+            new_fn = collected_filename(cfg, outdir)
             if path.isfile(new_fn):
                 if path.getsize(fn) == path.getsize(new_fn):
                     continue
 
             fn = path.abspath(fn)
             if copy:
-                copyfile(fn,new_fn)
+                copyfile(fn, new_fn)
             else:
                 try:
                     relative_symlink(fn, new_fn)
                 except:
-                    echo(_bullet('yellow')+" File exists", err=True)
+                    echo(_bullet("yellow") + " File exists", err=True)
 
             # Report progress
             mode = "Copied" if copy else "Symlinked"
-            echo(_bullet('green')+" "+mode+" "+_file(fn), err=True)
-            pad = " "*len(mode)
-            echo(pad+"to "+_file(new_fn), err=True)
+            echo(_bullet("green") + " " + mode + " " + _file(fn), err=True)
+            pad = " " * len(mode)
+            echo(pad + "to " + _file(new_fn), err=True)
 
     echo("Finished collecting figures", err=True)
